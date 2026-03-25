1d8384dba3861db5096feec03594f8673edafe01
Manjur-WD <mak985812@gmail.com>
2026-03-25 12:09:22 +0530

    updated

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -3,6 +3,7 @@ import { useRouter, useSearchParams } from "next/navigation";
 import { products } from "@/data/products";
 import Image from "next/image";
 import BreadCrumbContainer from "@/components/layouts/BreadCrumbContainer";
+import { Suspense } from "react";
 
 export default function ProductList() {
     const router = useRouter();
@@ -24,43 +25,46 @@ export default function ProductList() {
         : products.filter((p) => p.category === activeCategory);
 
     return (
-        <>
-            <BreadCrumbContainer breadcrumbLink="products" />
-            <section className="py-12 px-6 max-w-7xl mx-auto">
-                {/* FILTER BUTTONS */}
-                <div className="flex flex-wrap gap-2 justify-center mb-10 sticky top-25 z-10">
-                    {categories.map((cat) => (
-                        <button
-                            key={cat}
-                            onClick={() => handleCategoryChange(cat)}
-                            className={`md:px-5 px-3 md:py-2 py-1 md:text-xl text-[14px] rounded-full border font-bayon tracking-wider cursor-pointer transition ${activeCategory === cat ? "bg-[#892D1C] text-white" : "bg-white text-black"
-                                }`}
-                        >
-                            {cat.toUpperCase()}
-                        </button>
-                    ))}
-                </div>
+        <Suspense fallback={<div>Loading...</div>}>
+            <>
+                <BreadCrumbContainer breadcrumbLink="products" />
+                <section className="py-12 px-6 max-w-7xl mx-auto">
+                    {/* FILTER BUTTONS */}
+                    <div className="flex flex-wrap gap-2 justify-center mb-10 sticky top-25 z-10">
+                        {categories.map((cat) => (
+                            <button
+                                key={cat}
+                                onClick={() => handleCategoryChange(cat)}
+                                className={`md:px-5 px-3 md:py-2 py-1 md:text-xl text-[14px] rounded-full border font-bayon tracking-wider cursor-pointer transition ${activeCategory === cat ? "bg-[#892D1C] text-white" : "bg-white text-black"
+                                    }`}
+                            >
+                                {cat.toUpperCase()}
+                            </button>
+                        ))}
+                    </div>
 
-                {/* PRODUCT GRID */}
-                <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
-                    {filteredProducts.map((product) => (
-                        <div key={product.id} className="product-card relative group bg-white rounded-3xl text-center">
-                            <div className="p-5">
-                                <Image
-                                    src={`/images/products/${product.image}`}
-                                    alt={product.name}
-                                    height={500}
-                                    width={500}
-                                    className="w-full md:h-70 h-50 object-contain group-hover:scale-108 transition-all duration-300"
-                                />
+                    {/* PRODUCT GRID */}
+                    <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+                        {filteredProducts.map((product) => (
+                            <div key={product.id} className="product-card relative group bg-white rounded-3xl text-center">
+                                <div className="p-5">
+                                    <Image
+                                        src={`/images/products/${product.image}`}
+                                        alt={product.name}
+                                        height={500}
+                                        width={500}
+                                        className="w-full md:h-70 h-50 object-contain group-hover:scale-108 transition-all duration-300"
+                                    />
+                                </div>
+                                <h3 className="absolute group-hover:top-[90%] transition-all duration-300 top-[80%] left-1/2 -translate-x-1/2 bg-black text-white px-5 py-1 text-sm font-bayon uppercase">
+                                    {product.name.replace(/-/g, " ")}
+                                </h3>
                             </div>
-                            <h3 className="absolute group-hover:top-[90%] transition-all duration-300 top-[80%] left-1/2 -translate-x-1/2 bg-black text-white px-5 py-1 text-sm font-bayon uppercase">
-                                {product.name.replace(/-/g, " ")}
-                            </h3>
-                        </div>
-                    ))}
-                </div>
-            </section>
-        </>
+                        ))}
+                    </div>
+                </section>
+            </>
+        </Suspense>
+
     );
 }
