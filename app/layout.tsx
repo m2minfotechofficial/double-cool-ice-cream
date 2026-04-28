68666a34bceaa83a4dff099e53fe4f133a2c7973
Manjur-WD <mak985812@gmail.com>
2026-04-28 13:48:54 +0530

    updated

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,8 +7,7 @@ import SmoothWrapper from "@/components/layouts/SmoothWrapper";
 import Loader from "@/components/layouts/Loader";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 import TransitionProviders from "@/providers/TransitionProviders";
-import Head from "next/head";
-import Script from "next/script";
+import { GoogleAnalytics } from '@next/third-parties/google'
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -16,17 +15,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <Head>
-        {/* Google tag (gtag.js)  */}
-        <Script async src="https://www.googletagmanager.com/gtag/js?id=G-X8X19F44KN"></Script>
-        <Script>
-          {`window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-
-          gtag('config', 'G-X8X19F44KN');`}
-        </Script>
-      </Head>
       <body suppressHydrationWarning className={`${dmSans.variable} ${bayon.variable} ${baloo2.variable} ${carterOne.variable} ${anekBangla.variable} min-h-screen antialiased scroll-smooth touch-manipulation scrollbar-hide `}>
         <TransitionProviders>
           <Header />
@@ -39,6 +27,7 @@ export default function RootLayout({
           </SmoothWrapper>
         </TransitionProviders>
         <SpeedInsights />
+        <GoogleAnalytics gaId="G-X8X19F44KN" />
       </body>
     </html>
   );
